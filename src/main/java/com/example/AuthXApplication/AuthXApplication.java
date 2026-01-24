3f1cbce4b3b8c30e3ca3b0db65204c325030c774
pdschandel <pushpdeep12300@gmail.com>
2026-01-24 17:33:16 +0530

    Initila Commit

---
+++ src/main/java/com/example/AuthXApplication/AuthXApplication.java
@@ -0,0 +1,13 @@
+package com.example.AuthXApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AuthXApplication/AuthXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AuthXApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
